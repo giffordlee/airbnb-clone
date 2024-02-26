f55e562f1a638fbbff056388853c94a713158fd8
giffordlee <giffordlee20@gmail.com>
2024-02-26 20:24:25 +0800

    first commit

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,10 @@
+
+const IndexPage = () => {
+  return (
+    <div>
+      index
+    </div>
+  )
+}
+
+export default IndexPage
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const RegisterPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Register</h1>
+        <form className="max-w-md mx-auto">
+          <input type="text" placeholder="John Doe" />
+          <input type="email" placeholder="your@email.com" />
+          <input type="password" placeholder="password" />
+          <button className="primary">Register</button>
+          <div className="text-center py-2 text-gray-500">
+            Already a member?{" "}
+            <Link to={"/login"} className="underline text-black">
+              Log In
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
